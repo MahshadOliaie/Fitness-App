17b01e1ca7a425c642e03f7861ba76c803feaaa1
MahshadOliaie <mahshadoliaie@gmail.com>
2024-04-05 13:12:39 +0330

    add log counter

--- src/components/Food/Food.jsx
+++ src/components/Food/Food.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react'
 import CSS from './index.module.css'
 
-function Food({data}) {
+function Food({ data, setSelectedFoods }) {
+    const [selectState, setSelectState] = useState(false)
+
+    useEffect(() => {
+        if (selectState)
+            setSelectedFoods(prev => [...prev, data])
+        else
+            setSelectedFoods(prev => prev.filter(item => item != data))
+
+    }, [selectState])
+
     return (
         <>
             <div className={CSS.food}>
@@ -11,10 +22,12 @@ function Food({data}) {
                         <p className={CSS.serving}>{Math.round(data.serving_size_g)} gr</p>
                     </div>
 
-                    <div className={CSS.selectBox}></div>
+                    <div className={CSS.selectBox} onClick={() => setSelectState(prev => !prev)} style={selectState ? { backgroundColor: "green" } : {}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" /></svg>
+                    </div>
                 </div>
 
-            </div>
+            </div >
         </>
     )
 }
