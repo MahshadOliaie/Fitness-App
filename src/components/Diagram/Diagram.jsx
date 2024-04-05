1e575e841e1a2baaad5417a93887fb782e85a5af
MahshadOliaie <mahshadoliaie@gmail.com>
2024-04-05 12:37:02 +0330

    diagram shape changed

--- src/components/Diagram/Diagram.jsx
+++ src/components/Diagram/Diagram.jsx
@@ -3,8 +3,8 @@ import CSS from './index.module.css'
 function Diagram() {
     return (
         <>
-        <div className={CSS.diagramContainer}>
-            <svg className={CSS.diagram} width="246" height="159" viewBox="0 0 246 159" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <div className={CSS.diagramContainer}>
+                {/* <svg className={CSS.diagram} width="246" height="159" viewBox="0 0 246 159" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path className={CSS.baseline} d="M11.0032 157.484C11.003 110.997 11.0029 61.4971 27.001 41.9892C42.9992 22.4813 64.5007 11.487 120.001 11.487C175.501 11.487 201.5 21.5 215.501 42.4892C229.502 63.4784 235.003 102.996 235.003 158.442" />
 
                 <path className={CSS.protein} d="M11.0032 157.484C11.003 110.997 11.0029 61.4971 27.001 41.9892C42.9992 22.4813 64.5007 11.487 120.001 11.487C175.501 11.487 201.5 21.5 215.501 42.4892C229.502 63.4784 235.003 102.996 235.003 158.442" />
@@ -13,32 +13,53 @@ function Diagram() {
 
                 <path className={CSS.fat} d="M11.0032 157.484C11.003 110.997 11.0029 61.4971 27.001 41.9892C42.9992 22.4813 64.5007 11.487 120.001 11.487C175.501 11.487 201.5 21.5 215.501 42.4892C229.502 63.4784 235.003 102.996 235.003 158.442" />
 
-            </svg>
-            <div className={CSS.calories}>
-                <p className={CSS.caloryNum}>2334</p>
-                <p>kcal</p>
-            </div>
-        </div>
+            </svg> */}
+                <svg viewBox="0 0 36 36" className={CSS.diagram}>
+                    <path className={CSS.baseline} strokeDasharray="100" d="M18 2.0845
+                a 15.9155 15.9155 0 0 1 0 31.831
+                a 15.9155 15.9155 0 0 1 0 -31.831"
+                    />
+                    <path className={CSS.fatPath} strokeDasharray="100" d="M18 2.0845
+                a 15.9155 15.9155 0 0 1 0 31.831
+                a 15.9155 15.9155 0 0 1 0 -31.831"
+                    />
 
-        <div className={CSS.nutritionGuide}>
-            <div className={CSS.nutrition}>
-                <div className={CSS.protein}></div>
-                <p className={CSS.grams}>120g</p>
-                <p className={CSS.unit}>PROTEIN</p>
+                    <path className={CSS.carbsPath} strokeDasharray="100" d="M18 2.0845
+                a 15.9155 15.9155 0 0 1 0 31.831
+                a 15.9155 15.9155 0 0 1 0 -31.831"
+                    />
+
+                    <path className={CSS.proteinPath} strokeDasharray="100" d="M18 2.0845
+                a 15.9155 15.9155 0 0 1 0 31.831
+                a 15.9155 15.9155 0 0 1 0 -31.831"
+                    />
+
+                </svg>
+                <div className={CSS.calories}>
+                    <p className={CSS.caloryNum}>2334</p>
+                    <p className={CSS.left}>left</p>
+                </div>
             </div>
 
-            <div className={CSS.nutrition}>
-                <div className={CSS.carbs}></div>
-                <p className={CSS.grams}>70g</p>
-                <p className={CSS.unit}>CARBS</p>
-            </div>
+            <div className={CSS.nutritionGuide}>
+                <div className={CSS.nutrition}>
+                    <div className={CSS.protein}></div>
+                    <p className={CSS.grams}>120g</p>
+                    <p className={CSS.unit}>PROTEIN</p>
+                </div>
 
-            <div className={CSS.nutrition}>
-                <div className={CSS.fat}></div>
-                <p className={CSS.grams}>0g</p>
-                <p className={CSS.unit}>FAT</p>
+                <div className={CSS.nutrition}>
+                    <div className={CSS.carbs}></div>
+                    <p className={CSS.grams}>70g</p>
+                    <p className={CSS.unit}>CARBS</p>
+                </div>
+
+                <div className={CSS.nutrition}>
+                    <div className={CSS.fat}></div>
+                    <p className={CSS.grams}>0g</p>
+                    <p className={CSS.unit}>FAT</p>
+                </div>
             </div>
-        </div>
         </>
     )
 }
