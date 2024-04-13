129afb1f14805a69bf874d81b331e250d08bd75d
MahshadOliaie <mahshadoliaie@gmail.com>
2024-04-13 18:24:53 +0330

    add detail window

--- src/components/Meals/Meals.jsx
+++ src/components/Meals/Meals.jsx
@@ -1,32 +1,45 @@
+import { useState } from 'react'
 import useCalculator from '../../hooks/useCalculator'
+import MealDetail from '../MealDetail/MealDetail'
 import CSS from './index.module.css'
 
 
 function Meals() {
     const { breakfastInfo, lunchInfo, dinnerInfo, snackInfo } = useCalculator()
+    const [isDetailBoxOpen, setIsDetailBoxOpen] = useState(false)
+    const [boxHeader, setBoxHeader] = useState("")
+
+    function showDetail(meal) {
+        setBoxHeader(meal);
+        setIsDetailBoxOpen(true)
+    }
+
     return (
         <>
             <div className={CSS.meals}>
-                <div className={CSS.breakfast}>
+                <div className={CSS.breakfast} onClick={()=> showDetail("breakfast")}>
                     <p className={CSS.mealName}>breakfast</p>
                     <p className={CSS.cal}>{(breakfastInfo.calories).reduce((acc, curr) => acc + curr, 0)}</p>
                 </div>
                 <div className={CSS.devider}></div>
-                <div className={CSS.lunch}>
+                <div className={CSS.lunch} onClick={()=> showDetail("lunch")}>
                     <p className={CSS.mealName}>lunch</p>
                     <p className={CSS.cal}>{(lunchInfo.calories).reduce((acc, curr) => acc + curr, 0)}</p>
                 </div>
                 <div className={CSS.devider}></div>
-                <div className={CSS.dinner}>
+                <div className={CSS.dinner} onClick={()=> showDetail("dinner")}>
                     <p className={CSS.mealName}>dinner</p>
                     <p className={CSS.cal}>{(dinnerInfo.calories).reduce((acc, curr) => acc + curr, 0)}</p>
                 </div>
                 <div className={CSS.devider}></div>
-                <div className={CSS.snacks}>
+                <div className={CSS.snacks} onClick={()=> showDetail("snack")}>
                     <p className={CSS.mealName}>snacks</p>
                     <p className={CSS.cal}>{(snackInfo.calories).reduce((acc, curr) => acc + curr, 0)}</p>
                 </div>
             </div>
+
+            {isDetailBoxOpen && <MealDetail setIsDetailBoxOpen={setIsDetailBoxOpen} boxHeader={boxHeader} />}
+
         </>
     )
 }
