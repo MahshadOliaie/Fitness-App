a211cb1d962e8cc8da989546bc2f8672f4dd1c50
MahshadOliaie <mahshadoliaie@gmail.com>
2024-05-02 23:36:33 +0330

    move contexts to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import Dashboard from "./components/Dashboard/Dashboard"
-import FoodContext from "./context/FoodContext"
 import Layout from "./components/Layout/Layout"
 import LogHistory from "./context/LogHistory"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
@@ -11,51 +10,14 @@ import Profile from "./components/Profile/Profile"
 import StarterWizardForm from "./components/StarterWizardForm/StarterWizardForm"
 
 function App() {
-  const [breakfast, setBreakfast] = useState(JSON.parse(localStorage.getItem("breakfast")) || [])
-  const [lunch, setLunch] = useState(JSON.parse(localStorage.getItem("lunch")) || [])
-  const [dinner, setDinner] = useState(JSON.parse(localStorage.getItem("dinner")) || [])
-  const [snack, setSnack] = useState(JSON.parse(localStorage.getItem("snack")) || [])
-  const [water, setWater] = useState(JSON.parse(localStorage.getItem("water")) || 0)
-
-
   const [isFoodListOpen, setIsFoodListOpen] = useState(false)
   const [selectedMeal, setSelectedMeal] = useState("")
-  const [logHistory, setLogHistory] = useState(JSON.parse(localStorage.getItem("logHistory")) || [])
-
-  useEffect(() => {
-    localStorage.setItem("breakfast", JSON.stringify(breakfast))
-    localStorage.setItem("lunch", JSON.stringify(lunch))
-    localStorage.setItem("dinner", JSON.stringify(dinner))
-    localStorage.setItem("snack", JSON.stringify(snack))
-    localStorage.setItem("logHistory", JSON.stringify(logHistory))
-    localStorage.setItem("water", JSON.stringify(water))
-
-  }, [breakfast, lunch, dinner, snack, logHistory, water])
 
   return (
     <>
-      <FoodContext.Provider value={{
-        breakfast,
-        lunch,
-        dinner,
-        snack,
-        water,
-        setWater,
-        setBreakfast,
-        setDinner,
-        setLunch,
-        setSnack
-      }}>
-        <LogHistory.Provider value={{
-          logHistory,
-          setLogHistory
-        }}>
-
-
           <BrowserRouter>
             <Routes>
-              <Route path="/" element={<Layout setIsFoodListOpen={setIsFoodListOpen} setSelectedMeal={setSelectedMeal}>
-                <Dashboard setIsFoodListOpen={setIsFoodListOpen} selectedMeal={selectedMeal} isFoodListOpen={isFoodListOpen} /></Layout>}></Route>
+              <Route path="/" element={<Dashboard setSelectedMeal={setSelectedMeal} setIsFoodListOpen={setIsFoodListOpen} selectedMeal={selectedMeal} isFoodListOpen={isFoodListOpen} />}></Route>
               <Route path="/WaterPage" element={<Layout setIsFoodListOpen={setIsFoodListOpen} setSelectedMeal={setSelectedMeal}>
                 <WaterPage /></Layout>}></Route>
               <Route path="/Recipes" element={<Layout setIsFoodListOpen={setIsFoodListOpen} setSelectedMeal={setSelectedMeal}>
@@ -63,11 +25,8 @@ function App() {
               <Route path="/Profile" element={<Layout setIsFoodListOpen={setIsFoodListOpen} setSelectedMeal={setSelectedMeal}><Profile /></Layout>}></Route>
             </Routes>
           </BrowserRouter>
-
-
-        </LogHistory.Provider>
-      </FoodContext.Provider >
-      <StarterWizardForm />
+     
+      {/* <StarterWizardForm /> */}
     </>
   )
 }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,18 +2,56 @@
 import Diagram from "../Diagram/Diagram"
 import AddFoodWindow from "../AddFoodWindow/AddFoodWindow"
 import Meals from "../Meals/Meals"
+import { useEffect, useState } from "react"
+import Layout from "../Layout/Layout"
+import FoodContext from "../../context/FoodContext"
+import LogHistory from "../../context/LogHistory"
 
 
 
-function Dashboard({setIsFoodListOpen , selectedMeal , isFoodListOpen}) {
+function Dashboard({ setIsFoodListOpen, selectedMeal, isFoodListOpen, setSelectedMeal }) {
+    const [breakfast, setBreakfast] = useState(JSON.parse(localStorage.getItem("breakfast")) || [])
+    const [lunch, setLunch] = useState(JSON.parse(localStorage.getItem("lunch")) || [])
+    const [dinner, setDinner] = useState(JSON.parse(localStorage.getItem("dinner")) || [])
+    const [snack, setSnack] = useState(JSON.parse(localStorage.getItem("snack")) || [])
+    const [water, setWater] = useState(JSON.parse(localStorage.getItem("water")) || 0)
+    const [logHistory, setLogHistory] = useState(JSON.parse(localStorage.getItem("logHistory")) || [])
+
+    useEffect(() => {
+        localStorage.setItem("breakfast", JSON.stringify(breakfast))
+        localStorage.setItem("lunch", JSON.stringify(lunch))
+        localStorage.setItem("dinner", JSON.stringify(dinner))
+        localStorage.setItem("snack", JSON.stringify(snack))
+        localStorage.setItem("logHistory", JSON.stringify(logHistory))
+        localStorage.setItem("water", JSON.stringify(water))
+
+    }, [breakfast, lunch, dinner, logHistory, snack, water])
 
 
     return (
-        <>
-                <Diagram />
-                <Meals />
-                {isFoodListOpen && <AddFoodWindow setIsFoodListOpen={setIsFoodListOpen} selectedMeal={selectedMeal} />}
-        </>
+        <FoodContext.Provider value={{
+            breakfast,
+            lunch,
+            dinner,
+            snack,
+            water,
+            setWater,
+            setBreakfast,
+            setDinner,
+            setLunch,
+            setSnack
+        }}>
+            <LogHistory.Provider value={{
+                logHistory,
+                setLogHistory
+            }}>
+                <Layout setIsFoodListOpen={setIsFoodListOpen} setSelectedMeal={setSelectedMeal}>
+                    <Diagram />
+                    <Meals />
+                    {isFoodListOpen && <AddFoodWindow setIsFoodListOpen={setIsFoodListOpen} selectedMeal={selectedMeal} />}
+                </Layout>
+            </LogHistory.Provider>
+        </FoodContext.Provider >
     )
 }
 
