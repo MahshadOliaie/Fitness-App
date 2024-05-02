e9e5638402ebc03e0d686ca5bf9f5b2bdb1b685b
MahshadOliaie <mahshadoliaie@gmail.com>
2024-05-02 12:28:30 +0330

    add water vdiagram

--- src/components/WaterPage/WaterPage.jsx
+++ src/components/WaterPage/WaterPage.jsx
@@ -1,8 +1,19 @@
+import CSS from './index.module.css'
 
-
-function WaterPage(){
-    return(
-        <>coming soon</>
+function WaterPage() {
+    return (
+        <div className={CSS.container}>
+            <svg viewBox="0 0 36 36" className={CSS.diagram}>
+                <path className={CSS.baseline} strokeDasharray="100" d="M18 2.0845
+                a 15.9155 15.9155 0 0 1 0 31.831
+                a 15.9155 15.9155 0 0 1 0 -31.831"
+                />
+                <path className={CSS.waterPath} strokeDasharray="40 100" d="M18 2.0845
+                a 15.9155 15.9155 0 0 1 0 31.831
+                a 15.9155 15.9155 0 0 1 0 -31.831"
+                />
+            </svg>
+        </div>
     )
 }
 
