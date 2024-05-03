f4de0a691d872d1f57e487937113cf87a2763ddc
MahshadOliaie <mahshadoliaie@gmail.com>
2024-05-03 20:58:36 +0330

    editable name

--- src/components/Profile/UserImage/UserImage.jsx
+++ src/components/Profile/UserImage/UserImage.jsx
@@ -1,9 +1,17 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import CSS from './index.module.css'
 import Information from '../../../context/Information/Information'
+import { useForm } from 'react-hook-form'
 
 function UserImage() {
-    const { personalInfo } = useContext(Information)
+    const { personalInfo , setPersonalInfo } = useContext(Information)
+    const [edit, setEdit] = useState(false)
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    async function submit(data) {
+        setPersonalInfo({ ...personalInfo, "name": data.name })
+        await setEdit(false)
+    }
 
     return (
         <div className={CSS.imageContainer}>
@@ -11,11 +19,24 @@ function UserImage() {
                 <div className={CSS.userImage}>
                     <img src='/user.jpg' alt="" />
                 </div>
-                <div className={CSS.editBtn}>
-                    <svg className={CSS.editSvg} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" /></svg>
-                </div>
+                {(edit) ?
+                    <div className={CSS.doneBtn}>
+                        <p className={CSS.submitBtn} onClick={handleSubmit(submit)}>done</p>
+                    </div>
+                    :
+                    <div className={CSS.editBtn}>
+                        <svg className={CSS.editSvg} onClick={() => setEdit(true)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" /></svg>
+                    </div>
+                }
+
             </div>
+            {(edit)?
+            <input type="text" className={`${CSS.input} ${CSS.name}`} defaultValue={personalInfo.name} {...register("name", {
+                required: "fill this input"
+            })} style={(errors.name) ? { borderColor: "red" } : { borderColor: "black" }} />
+            :
             <h2 className={CSS.name}>{personalInfo.name}</h2>
+            }
         </div>
     )
 }
